76d338e57cd3747adf548655d6db4276846b25ed
Evhenii Holodniak <holodniak.e@gmail.com>
2015-12-30 20:25:37 +0200

    clients section with slider

--- app/js/common.js
+++ app/js/common.js
@@ -9,11 +9,28 @@ $(document).ready(function() {
 	// owl-carousel
   $("#owl-1, #owl-2").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      // autoPlay: 3000, //Set AutoPlay to 3 seconds
  
       items : 4,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
+ 
+  });
+
+    $("#owl-slider").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: 5000,
+      scrollPerPage : true
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
  
   });
 	//Check
